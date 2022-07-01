f14c6c20fe2e16292bda161adcfbade3faf06891
isaac <xuhonggang@hotmail.com>
2022-07-01 14:02:32 +0800

    ....

--- src/project.js
+++ src/project.js
@@ -10,7 +10,7 @@ export default {
     style: {
         theme: 'v2board',
         color: '#1890FF',
-        layout: 'topmenu',
+        layout: 'sidemenu',
         multipage: true,
         colorWeak: false,
         fixedHeader: false,
