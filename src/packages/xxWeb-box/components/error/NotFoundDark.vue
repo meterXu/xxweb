65704b44620cf0e1515054732b3232843598b991
hanjx <hanjx@dpark.com.cn>
2022-08-09 16:56:08 +0800

    ...

--- src/packages/xxWeb-box/components/error/NotFoundDark.vue
+++ src/packages/xxWeb-box/components/error/NotFoundDark.vue
@@ -3,8 +3,10 @@
     <div class="not-found-dark">
       <div class="not-found-left">
         <div class="code-dark">404</div>
-        <div class="info-top">Something is missing!</div>
+<!--        <div class="info-top">Something is missing!</div>-->
         <div class="info-dark">The page you are looking for does not exist, had it's name changed or is temporarilly unavailable.</div>
+        <span class="help-button">Help</span>
+        <span class="home-button">Home</span>
       </div>
       <div class="mark">
         <img src="../../assets/imgs/mark.png"></img>
