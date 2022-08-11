c1cbbf8594bb2e86719e05aa4a2a5749d533f033
hanjx <hanjx@dpark.com.cn>
2022-08-11 10:49:52 +0800

    ...

--- src/packages/xxWeb-utils/src/request.js
+++ src/packages/xxWeb-utils/src/request.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {getProject, globalStore} from './index'
 import axios from 'axios'
 import {ACCESS_TOKEN} from "./mutation-types"
-import {Modal, notification} from 'ant-design-vue'
+// import {Modal, notification} from 'ant-design-vue'
 import {LogFv,axiosRequest,axiosError} from '@dpark/logfv-web-vue'
 import {getLogFvConf} from './util'
 function createService(project,withCredentials,baseApiKey,isToken){
@@ -56,76 +56,6 @@ function initLog(project,logfv){
         logfv.initConfig(logConf)
     }
 }
-function error(error, project) {
-    if (error.response) {
-        let data = error.response.data;
-        let message = ""
-        if(typeof(data)==="string"&&data.indexOf('{')>-1){
-            data = JSON.parse(data);
-        }
-        if(data&&data.message){
-            message = data.message
-        }
-        const token = decodeURIComponent(Vue.ls.get(ACCESS_TOKEN));
-        console.log("------异常响应------", token);
-        console.log("------异常响应------", error.response.status);
-        switch (error.response.status) {
-            case 403:
-                notification.error({message: '系统提示', description: message||'拒绝访问', duration: 4});
-                break
-            case 500:
-                if (data.message == "Token失效，请重新登录") {
-                    loginExpired(project)
-                } else if(data.message.indexOf("用户名不存在或者密码错误")>-1){
-                    notification.error({message: '系统提示', description: '用户名不存在或者密码错误!', duration: 4});
-                } else {
-                    notification.error({message: '系统提示', description: message||'接口错误!', duration: 4});
-                }
-                break
-            case 404:
-                notification.error({message: '系统提示', description: message||'很抱歉，资源未找到!', duration: 4});
-                break
-            case 504:
-                notification.error({message: '系统提示', description: message||'网络超时'});
-                break
-            case 401:
-                loginExpired(project)
-                break
-            case 426:
-                notification.error({message: '系统提示', description: message||'服务器拒绝使用当前协议执行请求'});
-                break
-            default:
-                notification.error({
-                    message: '系统提示',
-                    description: data.message,
-                    duration: 4
-                });
-                break
-        }
-    }else{
-        notification.error({
-            message: '系统提示',
-            description: error.message,
-            duration: 4
-        })
-    }
-    return Promise.reject(error)
-}
-
-function loginExpired(project){
-    Modal.destroyAll();
-    Modal.error({
-        title: '登录已过期',
-        content: '很抱歉，登录已过期，请重新登录',
-        okText: '重新登录',
-        mask: false,
-        zIndex: 100000,
-        onOk: () => {
-            Logout(project)
-        }
-    });
-}
-
 function Logout(project){
     globalStore.dispatch('Logout').then(() => {
         Vue.ls.remove(ACCESS_TOKEN);
