d973896ba4c8406c423ced83dc91719de28c5ceb
xuhg <xuhg@dpark.com.cn>
2022-12-16 15:24:08 +0800

    fix bug

--- src/views/demo/Dashboard.vue
+++ src/views/demo/Dashboard.vue
@@ -88,24 +88,83 @@
       </el-col>
       <el-col :span="8">
         <el-card :body-style="{ padding: '0px' }">
-          <div class="min-card-chart" style="background: #FFF">
+          <div style="background: #FFF">
             <img src="https://materialpro-vue3-dark.vercel.app/assets/weatherbg.39f32caf.jpg" width="100%" height="90">
+            <div style="padding: 7px 20px;display: flex">
+              <div class="d-flex align-center justify-center">
+                <i class="el-icon-sunny weather-icon"></i>
+                <div class="mm-h1">
+                  <h1>32<sup>0</sup></h1>
+                  <small>Sunny Rainy day</small>
+                </div>
+              </div>
+              <div class="d-flex align-center justify-center">
+                <i class="el-icon-moon weather-icon" style="color: #0CB9C5FF"></i>
+                <div class="mm-h1">
+                  <h1>25<sup>0</sup></h1>
+                  <small>Tonight</small>
+                </div>
+              </div>
+            </div>
           </div>
         </el-card>
       </el-col>
     </el-row>
     <el-row class="row-margin card-no-border" :gutter="16">
       <el-col :span="8">
-        <el-card></el-card>
-        <el-card style="margin-top: 24px"></el-card>
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="https://materialpro-vue3-dark.vercel.app/assets/login-register.eee26258.jpg" width="100%">
+          <div class="user-avatar">
+            <el-avatar :size="100" src="https://materialpro-vue3-dark.vercel.app/assets/1.32ddcd87.jpg"></el-avatar>
+          </div>
+          <div class="user-content">
+            <h2 style="margin-top: 16px">Jonathan Dominic</h2>
+            <small>Web Designer & Developer</small>
+            <br/>
+            <el-button type="primary" style="border-radius: 50px;width: 100px;height: 36px;margin-top: 12px">Follow</el-button>
+            <el-row style="margin-top: 24px;padding-bottom: 16px">
+              <el-col :span="8">
+                <h2>1099</h2>
+                <small>Articles</small>
+              </el-col>
+              <el-col :span="8">
+                <h2>23,469</h2>
+                <small>Followers</small>
+              </el-col>
+              <el-col :span="8">
+                <h2>6035</h2>
+                <small>Following</small>
+              </el-col>
+            </el-row>
+          </div>
+        </el-card>
+        <el-card style="margin-top: 24px" :body-style="{ padding: '0px' }">
+          <div class="header">
+           <h2>My Contacts</h2>
+           <h5>Checkout my contacts here</h5>
+          </div>
+          <div class="pa-4">
+            <div class="li-user" v-for="item in 4">
+              <div class="li-user-avatar"><el-avatar :size="40" :src="`https://materialpro-vue3-dark.vercel.app/assets/images/users/${item}.jpg`"></el-avatar></div>
+              <div class="li-user-info">
+                <h4>Pavan Kumar</h4>
+                <h6>info@wrappixel.com</h6>
+              </div>
+            </div>
+          </div>
+        </el-card>
       </el-col>
       <el-col :span="16">
         <el-card>
-          <el-tabs>
-            <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-            <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-            <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-            <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+          <el-tabs v-model="activeName">
+            <el-tab-pane label="Activity" name="Activity">
+              <div class="profiletimeline">
+                <div></div>
+                <el-divider></el-divider>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="Profile" name="Profile">Profile</el-tab-pane>
+            <el-tab-pane label="Setting" name="Setting">Setting</el-tab-pane>
           </el-tabs>
         </el-card>
       </el-col>
@@ -129,6 +188,11 @@ export default {
     AreaChart,
     MiniLineChart,
     MinColumnChart
+  },
+  data(){
+    return {
+      activeName:'Activity'
+    }
   }
 }
 </script>
@@ -195,5 +259,77 @@ export default {
 }
 .tags .el-tag{
   margin-right: 8px;
+}
+.d-flex{
+  display: flex;
+  padding: 10px;
+  flex: 0.5;
+}
+.align-center{
+  align-items: center;
+}
+.justify-center{
+  justify-content: center;
+}
+.mm-h1{
+  margin-left: 12px;
+  line-height:20px;
+  h1{
+    font-size: 28px;
+  }
+}
+.weather-icon{
+  font-size: 45px;
+  font-weight: 550;
+  color: rgb(30,136,229);
+}
+sup {
+  font-size:75%
+}
+small {
+  font-size: 80%;
+}
+.user-avatar{
+  text-align: center;
+  margin-top: -60px;
+}
+.user-content{
+  padding: 20px 30px 30px;
+  text-align: center;
+}
+h1,h2,h5,h4,h6{
+  padding: 0;
+  margin: 0;
+  font-weight: 500;
+}
+
+h2{
+  font-size: 21px;
+}
+h5{
+  font-size: 12px;
+}
+.header{
+  padding: 20px;
+  background: rgb(30,136,229);
+}
+.pa-4{
+  padding: 16px;
+}
+.li-user{
+  display: flex;
+  padding: 12px 16px;
+  align-items: center;
+  h4,h6{
+    height: 24px;
+  }
+}
+.li-user-avatar{
+  margin-right: 12px;
+}
+.profiletimeline{
+  padding-left: 40px;
+  margin: 10px 10px 0 30px;
+  border-left: 1px solid rgba(0,0,0,.1);
 }
 </style>
