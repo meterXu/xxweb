f4e2a912101522c4b77db100dee46356d9256c55
isaac <xhgainxq@qq.com>
2023-01-12 11:10:28 +0800

    ....

--- src/packages/xxWeb-box/components/editor/render/data/theme/domThemefactory.js
+++ src/packages/xxWeb-box/components/editor/render/data/theme/domThemefactory.js
@@ -1,5 +1,5 @@
-// import chalk, {colorPalette as chalkColorPalette} from './echarts/chalk'
-// import blue, {colorPalette} from './echarts/blue'
+// import {colorPalette as chalkColorPalette} from './echarts/chalk'
+// import {colorPalette as blueColorPalette} from './echarts/blue'
 // import dark, {colorPalette} from './echarts/dark'
 // import darkblue, {colorPalette} from './echarts/dark-blue'
 // import yyjk, {colorPalette} from './echarts/yyjk'
@@ -13,85 +13,85 @@
 // import walden, {colorPalette} from './echarts/walden'
 // import westeros, {colorPalette} from './echarts/westeros'
 // import wonderland, {colorPalette} from './echarts/wonderland'
-//
-// export default {
-//   generateTheme: function (colorPalette) {
-//     colorPalette = colorPalette||{
-//       background: '#fff',
-//       color: '#fff',
-//       theme: [
-//         '#fff',
-//         '#BC3333',
-//         '#333',
-//         '#ddd'
-//       ],
-//       border: '#CCCCCC' }
-//     return {
-//       canvas: {
-//         backgroundColor: colorPalette.background
-//       },
-//       table: {
-//         primary: {
-//           background: colorPalette.background,
-//           borderColor: colorPalette.border
-//         },
-//         th: {
-//           color: colorPalette.theme[2],
-//           backgroundColor: colorPalette.theme[0],
-//           borderBottomColor: colorPalette.border,
-//           borderRightColor: colorPalette.border
-//         },
-//         td: {
-//           color: colorPalette.theme[1],
-//           backgroundColor: colorPalette.background,
-//           borderBottomColor: colorPalette.border,
-//           borderRightColor: colorPalette.border
-//         }
-//       },
-//       container: {
-//         title: {
-//           color: colorPalette.theme[2],
-//           background: {
-//             backgroundColor: colorPalette.theme[0]
-//           },
-//           border: {
-//             borderBottomColor: colorPalette.border
-//           }
-//         },
-//         background: {
-//           backgroundColor: colorPalette.background
-//         },
-//         border: {
-//           borderColor: colorPalette.border
-//         }
-//       },
-//       text: {
-//         text: {
-//           color: colorPalette.color
-//         },
-//         background: {
-//           color: colorPalette.background
-//         },
-//         border: {
-//           color: colorPalette.border
-//         }
-//       },
-//       slide: {
-//         pageColor: {
-//           normal: colorPalette.theme[1],
-//           active: colorPalette.theme[0]
-//         }
-//       }
-//     }
-//   }
-// }
-//
-//
+
+export default {
+  generateTheme: function (colorPalette) {
+    colorPalette = colorPalette||{
+      background: '#fff',
+      color: '#fff',
+      theme: [
+        '#fff',
+        '#BC3333',
+        '#333',
+        '#ddd'
+      ],
+      border: '#CCCCCC' }
+    return {
+      canvas: {
+        backgroundColor: colorPalette.background
+      },
+      table: {
+        primary: {
+          background: colorPalette.background,
+          borderColor: colorPalette.border
+        },
+        th: {
+          color: colorPalette.theme[2],
+          backgroundColor: colorPalette.theme[0],
+          borderBottomColor: colorPalette.border,
+          borderRightColor: colorPalette.border
+        },
+        td: {
+          color: colorPalette.theme[1],
+          backgroundColor: colorPalette.background,
+          borderBottomColor: colorPalette.border,
+          borderRightColor: colorPalette.border
+        }
+      },
+      container: {
+        title: {
+          color: colorPalette.theme[2],
+          background: {
+            backgroundColor: colorPalette.theme[0]
+          },
+          border: {
+            borderBottomColor: colorPalette.border
+          }
+        },
+        background: {
+          backgroundColor: colorPalette.background
+        },
+        border: {
+          borderColor: colorPalette.border
+        }
+      },
+      text: {
+        text: {
+          color: colorPalette.color
+        },
+        background: {
+          color: colorPalette.background
+        },
+        border: {
+          color: colorPalette.border
+        }
+      },
+      slide: {
+        pageColor: {
+          normal: colorPalette.theme[1],
+          active: colorPalette.theme[0]
+        }
+      }
+    }
+  }
+}
+
+
 // export default {
 //   'dark-blue':darkblue,
 //   'purple-passion':purplePassion,
 //   blue,
-//   chalk,
+//   chalk:chalkColorPalette,
 //   dark,
 //   yyjk,
 //   essos,
