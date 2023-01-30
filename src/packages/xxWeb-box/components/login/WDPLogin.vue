43c1a9e2b708783d3f77e06d5704cdc50a0d0fb4
isaac <xhgainxq@qq.com>
2023-01-30 09:31:30 +0800

    ....

--- src/packages/xxWeb-box/components/login/WDPLogin.vue
+++ src/packages/xxWeb-box/components/login/WDPLogin.vue
@@ -5,88 +5,92 @@
         <slot v-bind:config="config">
           <Row style="height: 100%">
             <Col style="height: 100%" :span="16">
-              <div class="left">
-                <div class="bg-header">
-                  <img :src="config.logo" class="logo" alt="logo">
-                  <span class="title">{{ config.login.title }}</span>
-                </div>
-                <img class="bg-img" src="../../assets/imgs/login3/slogan.svg" alt=""/>
-              </div>
+             <slot v-slot:login-container-left>
+               <div class="left">
+                 <div class="bg-header">
+                   <img :src="config.logo" class="logo" alt="logo">
+                   <span class="title">{{ config.login.title }}</span>
+                 </div>
+                 <img class="bg-img" src="../../assets/imgs/login3/slogan.svg" alt=""/>
+               </div>
+             </slot>
             </Col>
             <Col style="height: 100%" :span="8">
-              <div class="main">
-                <div class="login-header">
-                  <span v-if="config.login" class="title">{{ config.login.title }}</span>
+              <slot v-slot:login-container-right>
+                <div class="main">
+                  <div class="login-header">
+                    <span v-if="config.login" class="title">{{ config.login.title }}</span>
+                  </div>
+                  <Tabs v-model="loginState.activeName" @tab-click="handleClick">
+                    <TabPane name="password">
+                      <span slot="label" class="tab-pane-label">密码登录</span>
+                      <Form ref="loginForm-password" :model="form.password" :rules="rules.password">
+                        <slot name="form-item-prefix"></slot>
+                        <FormItem prop="username">
+                          <Input v-model="form.password.username" @keyup.enter.native="login"
+                                 :placeholder="placeholder.username"/>
+                        </FormItem>
+                        <FormItem prop="password">
+                          <Input type="password" v-model="form.password.password" @keyup.enter.native="login"
+                                 :placeholder="placeholder.password"/>
+                        </FormItem>
+                        <FormItem prop="verificationCode">
+                          <Row :gutter="10">
+                            <Col :span="16"><Input v-model="form.password.verificationCode" @keyup.enter.native="login"
+                                                   :placeholder="placeholder.verificationCode"/></Col>
+                            <Col :span="8">
+                              <template class="vc-img">
+                                <slot name="form-login-verificationCode">
+                                  <img class="vc-img" src="../../assets/imgs/login3/verification-code.png"/>
+                                </slot>
+                              </template>
+                            </Col>
+                          </Row>
+                        </FormItem>
+                        <slot name="form-item-suffix"></slot>
+                      </Form>
+                      <slot name="form-login-btn">
+                        <Button :loading="loginState.loading" :disabled="loginState.loading" class="login-btn"
+                                type="primary" @click="login">登 录
+                        </Button>
+                        <slot name="form-login-btn-other"></slot>
+                      </slot>
+                    </TabPane>
+                    <TabPane name="message">
+                      <span slot="label" class="tab-pane-label">短信登录</span>
+                      <Form ref="loginForm-message" :model="form.message" :rules="rules.message">
+                        <slot name="form-item-prefix"></slot>
+                        <FormItem prop="phone">
+                          <Input v-model="form.message.phone" @keyup.enter.native="login"
+                                 :placeholder="placeholder.phone"/>
+                        </FormItem>
+                        <FormItem prop="verificationCode">
+                          <Input v-model="form.message.verificationCode" @keyup.enter.native="login"
+                                 :placeholder="placeholder.verificationCode"/>
+                        </FormItem>
+                        <slot name="form-item-suffix"></slot>
+                      </Form>
+                      <slot name="form-login-btn">
+                        <Button :loading="loginState.loading" :disabled="loginState.loading" class="login-btn"
+                                type="primary" @click="login">登 录
+                        </Button>
+                        <slot name="form-login-btn-other"></slot>
+                      </slot>
+                    </TabPane>
+                  </Tabs>
+                  <slot name="login-footer">
+                    <div class="login-footer">
+                      <span class="register" @click="registry">账号注册</span>
+                      <span class="certified" @click="enterpriseCertification">企业认证</span>
+                      <span class="forgot-btn" @click="forgotPassword">忘记密码</span>
+                    </div>
+                    <Divider direction="horizontal" content-position="center">第三方账号登录</Divider>
+                    <div class="third-party">
+                      <img class="third-party-img" src="../../assets/imgs/login3/weixin.svg" @click="wxLogin"/>
+                    </div>
+                  </slot>
                 </div>
-                <Tabs v-model="loginState.activeName" @tab-click="handleClick">
-                  <TabPane name="password">
-                    <span slot="label" class="tab-pane-label">密码登录</span>
-                    <Form ref="loginForm-password" :model="form.password" :rules="rules.password">
-                      <slot name="form-item-prefix"></slot>
-                      <FormItem prop="username">
-                        <Input v-model="form.password.username" @keyup.enter.native="login"
-                               :placeholder="placeholder.username"/>
-                      </FormItem>
-                      <FormItem prop="password">
-                        <Input type="password" v-model="form.password.password" @keyup.enter.native="login"
-                               :placeholder="placeholder.password"/>
-                      </FormItem>
-                      <FormItem prop="verificationCode">
-                        <Row :gutter="10">
-                          <Col :span="16"><Input v-model="form.password.verificationCode" @keyup.enter.native="login"
-                                                 :placeholder="placeholder.verificationCode"/></Col>
-                          <Col :span="8">
-                            <template class="vc-img">
-                              <slot name="form-login-verificationCode">
-                                <img class="vc-img" src="../../assets/imgs/login3/verification-code.png"/>
-                              </slot>
-                            </template>
-                          </Col>
-                        </Row>
-                      </FormItem>
-                      <slot name="form-item-suffix"></slot>
-                    </Form>
-                    <slot name="form-login-btn">
-                      <Button :loading="loginState.loading" :disabled="loginState.loading" class="login-btn"
-                              type="primary" @click="login">登 录
-                      </Button>
-                      <slot name="form-login-btn-other"></slot>
-                    </slot>
-                  </TabPane>
-                  <TabPane name="message">
-                    <span slot="label" class="tab-pane-label">短信登录</span>
-                    <Form ref="loginForm-message" :model="form.message" :rules="rules.message">
-                      <slot name="form-item-prefix"></slot>
-                      <FormItem prop="phone">
-                        <Input v-model="form.message.phone" @keyup.enter.native="login"
-                               :placeholder="placeholder.phone"/>
-                      </FormItem>
-                      <FormItem prop="verificationCode">
-                        <Input v-model="form.message.verificationCode" @keyup.enter.native="login"
-                               :placeholder="placeholder.verificationCode"/>
-                      </FormItem>
-                      <slot name="form-item-suffix"></slot>
-                    </Form>
-                    <slot name="form-login-btn">
-                      <Button :loading="loginState.loading" :disabled="loginState.loading" class="login-btn"
-                              type="primary" @click="login">登 录
-                      </Button>
-                      <slot name="form-login-btn-other"></slot>
-                    </slot>
-                  </TabPane>
-                </Tabs>
-                <slot name="login-footer">
-                  <div class="login-footer">
-                    <span class="register" @click="registry">账号注册</span>
-                    <span class="certified" @click="enterpriseCertification">企业认证</span>
-                    <span class="forgot-btn" @click="forgotPassword">忘记密码</span>
-                  </div>
-                  <Divider direction="horizontal" content-position="center">第三方账号登录</Divider>
-                  <div class="third-party">
-                    <img class="third-party-img" src="../../assets/imgs/login3/weixin.svg" @click="wxLogin"/>
-                  </div>
-                </slot>
-              </div>
+              </slot>
             </Col>
           </Row>
         </slot>
