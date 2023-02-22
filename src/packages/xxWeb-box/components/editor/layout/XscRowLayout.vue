550c2d4c5a9acc7c3cb9b128bc87a6670aadee95
isaac <xhgainxq@qq.com>
2023-02-22 21:55:11 +0800

    ...

--- src/packages/xxWeb-box/components/editor/layout/XscRowLayout.vue
+++ src/packages/xxWeb-box/components/editor/layout/XscRowLayout.vue
@@ -1,18 +1,24 @@
 <template>
-  <el-row :justify="10">
-    <el-col :span="24">1</el-col>
-    <el-col :span="24">2</el-col>
-    <el-col :span="24">3</el-col>
-    <el-col :span="24">4</el-col>
-    <el-col :span="24">5</el-col>
-    <el-col :span="24">6</el-col>
-  </el-row>
+  <draggable
+      class="list-group"
+      v-bind="dragOptions"
+      @start="drag = true"
+      @end="drag = false"
+  >
+    <el-row :gutter="0" v-for="item in charts">
+      <el-col :span="24" class="xsc-item">
+        <slot :view="view" :item="item"></slot>
+      </el-col>
+    </el-row>
+  </draggable>
 </template>
 
 <script>
+import draggable from "vuedraggable";
+
 export default {
   name: "XscRowLayout",
-  props:{
+  props: {
     scale: Number,
     charts: {
       type: Array,
@@ -22,18 +28,53 @@ export default {
       type: Boolean,
       default: true
     },
-    activeItem:{
-      type:Object,
-      default:null
+    activeItem: {
+      type: Object,
+      default: null
     },
   },
-  model:{
-    prop:'activeItem',
-    event:'change'
+  model: {
+    prop: 'activeItem',
+    event: 'change'
   },
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      dragOptions: {
+        animation: 200,
+        group: "description",
+        disabled: this.view,
+        ghostClass: "ghost"
+      }
+    }
+  },
+  methods:{
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.xsc-item {
+  padding: 6px;
+  cursor: grab;
+  position: relative;
+  box-sizing: border-box;
+}
+.ghost {
+  border:2px dashed #acacac;
+  :after{
+    content: '';
+    display: block;
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgb(161, 255, 235);
+  }
 
+}
 </style>
--- src/views/demo/RowLayoutEditor.vue
+++ src/views/demo/RowLayoutEditor.vue
@@ -1,20 +1,55 @@
 <template>
-  <MtView>
-    <XscRowLayout></XscRowLayout>
-  </MtView>
+  <div class="editor">
+    <MtView :config="config">
+      <XscRowLayout :charts="charts" :view="false">
+        <div style="width: 800px;height: 200px;background: green"></div>
+      </XscRowLayout>
+    </MtView>
+  </div>
 </template>
 
 <script>
 import {MtView,XscRowLayout} from "../../packages/xxWeb-box";
+import charts from './charts'
 export default {
   name: "RowLayoutEditor",
   components:{
     MtView,
     XscRowLayout
+  },
+  data(){
+    return {
+      charts:charts,
+      config:{
+        theme:'light',
+        isRuler:false,
+        isScale:true,
+        isDrag:true,
+        isNavigate:true,
+        isAlign:false,
+        containerPadding:30,
+        initScale:1,
+        backgroundClass:'default-bg',
+        navigateConf:{
+          canvasWidth:224,
+          canvasHeight:160
+        }
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.editor{
+  display: flex;
+  overflow: hidden;
+  flex: 1;
+  position: absolute;
+  top: 40px;
+  bottom: 0;
+  right: 0;
+  left: 1px;
+  z-index: 99;
+}
 </style>
