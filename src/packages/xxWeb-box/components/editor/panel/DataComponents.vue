cbe742299c10af052f8f6aa997b7b38fbcc7435c
zhangz <zbrbz@outlook.com>
2023-02-27 17:34:33 +0800

    feat: 扩展面板数据功能项

--- src/packages/xxWeb-box/components/editor/panel/DataComponents.vue
+++ src/packages/xxWeb-box/components/editor/panel/DataComponents.vue
@@ -1,25 +1,41 @@
 <template>
   <div class="data-components">
     <div class="top-radio">
-      <el-radio v-model="value.source.type" :label="2">静态数据</el-radio>
-      <el-radio v-model="value.source.type" :label="3">动态数据</el-radio>
+      <el-radio-group v-model="value.source.type" @change='changeSourceType'>
+        <el-radio :label="2">静态数据</el-radio>
+        <el-radio :label="3">动态数据</el-radio>
+      </el-radio-group>
     </div>
-    <div v-show="value.source.type===2" class="static-data">
-      <div v-if="value.type==='echarts'" class="spreadsheet">
+    <div v-show="value.source.type === 2" class="static-data">
+      <div v-if="['echarts', 'staticList'].includes(value.type)" class="spreadsheet">
         <el-button size="small" @click="handleSheet">配置数据</el-button>
+      </div>
+      <!-- 静态输入框 -->
+      <div v-else-if="value.type === 'staticInput'" class="spreadsheet">
+        <el-input v-model="value.source.json" size="mini" clearable @change="changeData"></el-input>
+      </div>
+
+      <!-- 静态表单 -->
+      <div v-else-if="value.type === 'staticForm'" class="spreadsheet">
+        <!-- <el-input v-model="value.source.json" size="mini" clearable @change="changeData"></el-input> -->
+        <el-form ref="form" :inline="true" :model="value.source.json" label-width="40px">
+          <el-form-item :label="item.label" v-for='item in formoption' :key='item.key'>
+            <el-input v-model="value.source.json[item.key]"></el-input>
+          </el-form-item>
+        </el-form>
       </div>
       <div v-else class="static-form">
         DOM数据
       </div>
-    </div >
-    <div v-show="value.source.type===3" class="dynamic-data">
+    </div>
+    <div v-show="value.source.type === 3" class="dynamic-data">
       <el-form label-position="left" label-width="60px" :model="source" size="small">
         <el-form-item class="none-label">
           <el-row type="flex" justify="space-between">
             <el-col :span="12">
               <el-checkbox v-model="source.autoupdate">自动更新请求</el-checkbox>
             </el-col>
-            <el-col v-if="source.autoupdate" :span="11" style="display: flex;justify-content: flex-end">
+            <el-col v-show="source.autoupdate" :span="11" style="display: flex;justify-content: flex-end">
               <el-input v-model.number="source.autoupdateTime"></el-input>
               <span style="width: 60px;margin-left: 4px;font-size: 12px">秒/次</span>
             </el-col>
@@ -27,7 +43,7 @@
         </el-form-item>
         <el-form-item>
           <span slot="label">数据服务</span>
-          <span style="font-size: 12px;">{{source.method}}</span>
+          <span style="font-size: 12px;">{{ source.method }}</span>
         </el-form-item>
         <el-form-item>
           <span slot="label">Url</span>
@@ -55,28 +71,34 @@ import 'codemirror/mode/javascript/javascript.js'
 import tableDialog from './tableDialog'
 export default {
   name: "DataComponents",
-  props:['value'],
-  model:{
-    prop:'value',
-    event:'change'
+  props: ['value', 'formoption'],
+  model: {
+    prop: 'value',
+    event: 'change'
   },
   components: {
     tableDialog,
     codemirror
   },
-  watch:{
-    'value.source':{
-      deep:true,
+  watch: {
+    'value.source': {
+      deep: true,
       handler(nv) {
-        this.source = Object.assign({},nv)
-        this.tableData = JSON.parse(this.value.source.json)
+        this.source = Object.assign({}, nv)
+        if (['echarts', 'staticList'].includes(this.value.type)) {
+          this.tableData = JSON.parse(this.value.source.json)
+        } else {
+          this.tableData = this.value.source.json
+        }
       }
     }
   },
   data() {
     return {
+      // 配置为静态 input框数据
+      staticInput: '',
       dialogVisible: false,
-      tableData:[],
+      tableData: [],
       hotSettings: {
         colHeaders: true,
         rowHeaders: true,
@@ -95,31 +117,52 @@ export default {
       source: {}
     }
   },
-  methods:{
+  methods: {
+    // 切换静态数据和动态数据按钮
+    // 2 静态
+    // 3 动态
+    changeSourceType(val) {
+      // 切换为静态时存储一下临时数据
+      if (val === 2) {
+        this.value.source.dynamicJson = this.value.source.json
+        this.value.source.json = this.value.source.staticJson
+      } else {
+        this.value.source.staticJson = this.value.source.json
+        this.value.source.json = this.value.source.dynamicJson
+      }
+    },
     handleSheet() {
       this.dialogVisible = true
     },
     changeData(data) {
-      this.value.source.json = JSON.stringify(data)
+      if (['echarts', 'staticList'].includes(this.value.type)) {
+        this.value.source.json = JSON.stringify(data)
+      } else {
+        this.value.source.json = data
+      }
     },
     handleDynamicData() {
-      this.$emit('updateData',{
-        url:this.source.url,
-        method:this.source.method,
-        autoupdate:this.source.autoupdate,
+      this.$emit('updateData', {
+        url: this.source.url,
+        method: this.source.method,
+        autoupdate: this.source.autoupdate,
         autoupdateTime: this.source.autoupdateTime,
         type: this.value.type,
       })
     }
   },
   updated() {
-    if(this.value.source.type==3) {
+    if (this.value.source.type == 3) {
       this.$refs.cmExpressionsRef.refresh()
     }
   },
   mounted() {
-    this.source = Object.assign({},this.value.source)
-    this.tableData = JSON.parse(this.value.source.json)
+    this.source = Object.assign({}, this.value.source)
+    if (['echarts', 'staticList'].includes(this.value.type)) {
+      this.tableData = JSON.parse(this.value.source.json)
+    } else {
+      this.tableData = this.value.source.json
+    }
   }
 }
 </script>
@@ -128,10 +171,12 @@ export default {
 .none-label /deep/ .el-form-item__content {
   margin-left: 0 !important;
 }
+
 .data-components {
   width: 100%;
   height: 100%;
 }
+
 /deep/ .el-input--small .el-input__inner {
   height: 26px;
   line-height: 26px;
