e98336051599007a34134419856653d972b12a4b
isaac <xhgainxq@qq.com>
2023-03-07 14:46:39 +0800

    ...

--- src/packages/xxWeb-box/components/editor/UIList/UIList.vue
+++ src/packages/xxWeb-box/components/editor/UIList/UIList.vue
@@ -15,7 +15,9 @@
     >
         <span class="custom-tree-node" slot-scope="{ node, data }">
           <div class="custom-tree-node-label" @click="nodeClick(data)" @contextmenu="contextmenu(data)">
-            {{ node.label }}
+           <slot name="label" :data="data">
+              {{ data.name }}
+           </slot>
           </div>
           <slot name="ui-custom-icon" :data="{node,data}"></slot>
         </span>
@@ -78,8 +80,8 @@ export default {
     this.dataList = Object.assign([],this.uiList)
   },
   methods: {
-    handleDrop() {
-      this.$emit('nodeChange',this.dataList,'drag')
+    handleDrop(draggingNode, dropNode) {
+      this.$emit('nodeChange',this.dataList,'drag',dropNode.parent.data.id)
     },
     allowDrop(draggingNode, dropNode, type) {
       if (draggingNode.data.id !== dropNode.data.id) {
