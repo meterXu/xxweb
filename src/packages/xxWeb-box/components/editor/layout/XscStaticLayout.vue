7c5659b3f971ecde226acdddc6e0010a9ab5f78e
isaac <xhgainxq@qq.com>
2023-03-08 10:58:52 +0800

    ....

--- src/packages/xxWeb-box/components/editor/layout/XscStaticLayout.vue
+++ src/packages/xxWeb-box/components/editor/layout/XscStaticLayout.vue
@@ -15,11 +15,11 @@
          @dragstart="()=>{return false}">
       <slot v-if="item.config.box.show" :view="view" :item="item" :active="activeItem&&item.id===activeItem.id"></slot>
       <span style="user-select: none" draggable="false"  @mousedown="changeSizeSizeMousedown(item)">
-            <div ref="resize" v-if="!view&&activeItem&&item.id===activeItem.id" class="item_resize"></div>
+            <div ref="resize" v-if="!view&&(activeItem&&item.id===activeItem.id)" class="item_resize"></div>
       </span>
     </div>
     <span v-if="page.config.options.changeSize" draggable="false" style="user-select: none" @mousedown="changeSizeSizeMousedown(page)">
-        <div ref="resize" v-if="!view&&activeItem&&activeItem.chart==='canvas'" class="item_resize"></div>
+        <div ref="resize" v-if="!view&&(activeItem&&activeItem.chart==='canvas')" class="item_resize"></div>
     </span>
   </div>
 </template>
