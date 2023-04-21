ff2960f32d27e839903ba05a3e3901c56e2a0bc7
xuhg <xuhg@dpark.com.cn>
2023-04-21 09:51:49 +0800

    ....

--- src/packages/xxWeb-box/components/editor/view/RulerScale.vue
+++ src/packages/xxWeb-box/components/editor/view/RulerScale.vue
@@ -44,16 +44,13 @@ export default {
         this.guides2.scroll(-nv.y/this.scale,this.scale);
       }
     },
-    scale(nv,ov){
-      this.guides1.zoom  = nv
-      this.guides1.resize()
-      this.guides2.zoom  = nv
-      this.guides2.resize()
-      this.guides1.scroll(-this.location.x/nv,nv);
-      this.guides2.scroll(-this.location.y/nv,nv);
-      this.guides1.scrollGuides(-this.location.y/nv,nv);
-      this.guides2.scrollGuides(-this.location.x/nv,nv);
-
+    scale:{
+      handler(nv,ov){
+        this.$nextTick(()=>{
+          this.initGuides()
+        })
+      },
+      immediate:true
     }
   },
   computed: {
@@ -65,35 +62,34 @@ export default {
     onChangeLeft(guides) {
       this.lines.left = guides
     },
-  },
-  mounted() {
-    let optionsH = {
-      type: "horizontal",
-      displayDragPos: true,
-      zoom: this.scale,
-    }
-    let optionsV = {
-      type: "vertical",
-      displayDragPos: true,
-      zoom: this.scale,
-    }
-    if(this.theme==='light') {
-      optionsH.textColor= '#B5B5B5'
-      optionsH.backgroundColor= '#F1F4F8'
-      optionsH.lineColor = '#ddd'
-      optionsH.height=24
-      optionsH.textOffset = [0,4]
-      optionsV.textColor= '#B5B5B5'
-      optionsV.lineColor = '#ddd'
-      optionsV.backgroundColor= '#F1F4F8'
-      optionsV.width=24
-      optionsV.textOffset = [4,0]
-    } else {
-      optionsH.textColor= '#96A2BA'
-      optionsH.backgroundColor= '#1E2532'
-      optionsV.textColor= '#96A2BA'
-      optionsV.backgroundColor= '#1E2532'
-    }
+    initGuides(){
+      let optionsH = {
+        type: "horizontal",
+        displayDragPos: true,
+        zoom: this.scale,
+      }
+      let optionsV = {
+        type: "vertical",
+        displayDragPos: true,
+        zoom: this.scale,
+      }
+      if(this.theme==='light') {
+        optionsH.textColor= '#B5B5B5'
+        optionsH.backgroundColor= '#F1F4F8'
+        optionsH.lineColor = '#ddd'
+        optionsH.height=24
+        optionsH.textOffset = [0,4]
+        optionsV.textColor= '#B5B5B5'
+        optionsV.lineColor = '#ddd'
+        optionsV.backgroundColor= '#F1F4F8'
+        optionsV.width=24
+        optionsV.textOffset = [4,0]
+      } else {
+        optionsH.textColor= '#96A2BA'
+        optionsH.backgroundColor= '#1E2532'
+        optionsV.textColor= '#96A2BA'
+        optionsV.backgroundColor= '#1E2532'
+      }
       this.guides1 =new Guides(document.querySelector(".ruler-container-top"), optionsH).on("changeGuides", ({ guides }) => {
         this.onChangeTop(guides)
       });
@@ -102,6 +98,15 @@ export default {
       });
       this.guides1.loadGuides(this.lines.top)
       this.guides2.loadGuides(this.lines.left)
+      this.guides1.zoom  = nv
+      this.guides1.resize()
+      this.guides2.zoom  = nv
+      this.guides2.resize()
+      this.guides1.scroll(-this.location.x/nv,nv);
+      this.guides2.scroll(-this.location.y/nv,nv);
+      this.guides1.scrollGuides(-this.location.y/nv,nv);
+      this.guides2.scrollGuides(-this.location.x/nv,nv);
+    }
   }
 }
 </script>
