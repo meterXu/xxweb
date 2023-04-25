1c20e1bb5dfab2459b3c755995ad9442207e6841
xuhg <xuhg@dpark.com.cn>
2023-04-25 15:38:49 +0800

    ....

--- src/packages/xxWeb-box/components/editor/panel/MtFormColor.vue
+++ src/packages/xxWeb-box/components/editor/panel/MtFormColor.vue
@@ -5,10 +5,10 @@
         <el-color-picker color-format="hex" v-model="color" size="mini" :predefine="formatPredefine"
           @active-change='activeChange' @change='changeColor'></el-color-picker>
       </el-col>
-      <el-col :span='10' v-if='fItem.hasInput && !fItem.hideColor' class='text-col-twice' style="padding-right: 5px">
+      <el-col :span='12' v-if='fItem.hasInput && !fItem.hideColor' class='text-col-twice' style="padding-right: 5px">
         <el-input v-model='color' size='mini'></el-input>
       </el-col>
-      <el-col :span='fItem.opcSpan || 10' v-if='fItem.hasInput' class='prepend-percent' style="padding-left: 5px">
+      <el-col :span='fItem.opcSpan || 8' v-if='fItem.hasInput' class='prepend-percent' style="padding-left: 5px">
         <el-input v-model='opacity' size='mini' @input='changeColor'>
         </el-input>
       </el-col>
