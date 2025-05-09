bba1ce6dc1b3c3bc33211417097bcdbe9a5ccbda
isaac <xhgainxq@qq.com>
2025-05-09 08:52:35 +0800

    ...

--- src/packages/xxWeb-util/lib/filter.js
+++ src/packages/xxWeb-util/lib/filter.js
@@ -7,64 +7,54 @@ import {ACCESS_TOKEN,PERMISSION} from "./types";
  * @param project Project
  * @param beforeCallback 钩子执行之前
  * @param endCallback 钩子执行之后
+ * @param permission 是否权限验证
  */
-function filter(router, project,{beforeCallback,endCallback}) {
+function filter(router, project,{beforeCallback,endCallback,permission}) {
     let defaultLogin = project.redirect.login
     const whiteList = [defaultLogin, project.redirect['404'],project.redirect['403']]
     const _ls = new Ls(project.nameSpace)
     router.beforeEach((to, from, next) => {
         beforeCallback&&beforeCallback()
-        if (!to.matched.length) {
-            next({
-                path: project.redirect['404']
-            })
-            endCallback&&endCallback()
-            return false
-        }
-        if (to.query.action === 'logout') {
-            _ls.remove(ACCESS_TOKEN)
-        } else {
-            const accessToken = getQueryVariable(project.variable.tokenKey) || to.query[project.variable.tokenKey]
-            if (accessToken) {
-                _ls.set(ACCESS_TOKEN, accessToken)
-            }
-        }
-        if (whiteList.indexOf(to.path) >= 0) {
-            next()
-            endCallback&&endCallback()
-        } else if (_ls.get(PERMISSION)&&!validatePermission(to.path,_ls.get(PERMISSION))){
-            next({
-                path: project.redirect['403']||project.redirect['404']
-            })
-            endCallback&&endCallback()
-        } else {
-            let accessToken = _ls.get(ACCESS_TOKEN)
-            if (from.query.path!==to.path){
-                to.query.path = from.query.path
-            }
-            if (to.meta.requireAuth === false) {
-                next()
-                endCallback&&endCallback()
-            } else if (accessToken) {
-                next()
-                endCallback&&endCallback()
-            } else {
-                next({
-                    path: defaultLogin,
-                    query: Object.assign({}, to.query, {path: to.path})
-                })
-                endCallback&&endCallback()
-            }
+        validateNotFound(to,next,project,{endCallback})
+        dealWithQuery(to,_ls,project)
+        if(dealWithPerm(to,next,whiteList,_ls,permission,project,{endCallback})){
+            validateToken(to,from,next,_ls,defaultLogin,{endCallback})
         }
     })
 }
-
-/**
- * 权限验证
- * @param path 路径
- * @param permission 权限树
- * @return {boolean} true有权限，false无权限
- */
+function validateNotFound(to,next,project,{endCallback}){
+    if (!to.matched.length) {
+        next({
+            path: project.redirect['404']
+        })
+        endCallback&&endCallback()
+        return false
+    }
+}
+function dealWithQuery(to,ls,project){
+    if (to.query.action === 'logout') {
+        ls.remove(ACCESS_TOKEN)
+    } else {
+        const accessToken = getQueryVariable(project.variable.tokenKey) || to.query[project.variable.tokenKey]
+        if (accessToken) {
+            ls.set(ACCESS_TOKEN, accessToken)
+        }
+    }
+}
+function dealWithPerm(to,next,whiteList,ls,permission,project,{endCallback}){
+    if (whiteList.indexOf(to.path) >= 0) {
+        next()
+        endCallback&&endCallback()
+        return false
+    } else if (permission&&!validatePermission(to.path,ls.get(PERMISSION))){
+        next({
+            path: project.redirect['403']||project.redirect['404']
+        })
+        endCallback&&endCallback()
+        return false
+    }
+    return true
+}
 function validatePermission(path,permission){
     let res = false
     if(!permission){
@@ -89,5 +79,25 @@ function validatePermission(path,permission){
     return res
 
 }
-
+function validateToken(to,from,next,ls,defaultLogin,{endCallback}){
+    if (from.query.path!==to.path){
+        to.query.path = from.query.path
+    }
+    if (to.meta&&to.meta.requireAuth === false) {
+        next()
+        endCallback&&endCallback()
+        return false
+    } else if (ls.get(ACCESS_TOKEN)) {
+        next()
+        endCallback&&endCallback()
+        return false
+    } else {
+        next({
+            path: defaultLogin,
+            query: Object.assign({}, to.query, {path: to.path})
+        })
+        endCallback&&endCallback()
+        return false
+    }
+}
 export default filter
