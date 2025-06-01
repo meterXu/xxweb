68770f7435a8fd3aeb74d1a75ce588de434c685e
isaac <xhgainxq@qq.com>
2025-06-01 15:56:18 +0800

    代码重构

---
+++ src/packages/xxWeb-box/components/index.js
@@ -0,0 +1,31 @@
+import XXWebBox from "./XXWebBox.vue";
+import Logo from "./common/Logo.vue";
+import DynamicMenu from "./common/DynamicMenu.vue";
+import ItemLink from "./common/ItemLink.vue";
+import UserMenu from "./layouts/header/UserMenu.vue";
+import SearchMenu from "./layouts/header/SearchMenu.vue";
+import HeadTitle from "./layouts/header/HeadTitle.vue";
+import Hamburger from "./layouts/header/Hamburger.vue";
+import HeadBreadcrumb from "./layouts/header/HeadBreadcrumb.vue";
+import FullScreen from "./layouts/header/FullScreen.vue";
+import DrawerMenu from "./layouts/left/DrawerMenu.vue";
+import SideMenu from "./layouts/left/SideMenu.vue";
+import AppMain from "./layouts/main/AppMain.vue";
+import TabsView from "./layouts/main/TabsView.vue";
+
+export {
+    XXWebBox,
+    SideMenu,
+    UserMenu,
+    SearchMenu,
+    HeadTitle,
+    Hamburger,
+    HeadBreadcrumb,
+    Logo,
+    FullScreen,
+    DrawerMenu,
+    DynamicMenu,
+    ItemLink,
+    AppMain,
+    TabsView
+}
--- src/packages/xxWeb-box/index.js
+++ src/packages/xxWeb-box/index.js
@@ -1,5 +1,38 @@
-import XXWebBox from './components/XXWebBox.vue'
+import {
+    XXWebBox,
+    SideMenu,
+    UserMenu,
+    SearchMenu,
+    HeadTitle,
+    Hamburger,
+    HeadBreadcrumb,
+    Logo,
+    FullScreen,
+    DrawerMenu,
+    DynamicMenu,
+    ItemLink,
+    AppMain,
+    TabsView
+} from './components/index'
+
 XXWebBox.install = function (Vue){
     Vue.component(XXWebBox.__name,XXWebBox)
+}
+
+export {
+    XXWebBox,
+    SideMenu,
+    UserMenu,
+    SearchMenu,
+    HeadTitle,
+    Hamburger,
+    HeadBreadcrumb,
+    Logo,
+    FullScreen,
+    DrawerMenu,
+    DynamicMenu,
+    ItemLink,
+    AppMain,
+    TabsView
 }
 export default XXWebBox
